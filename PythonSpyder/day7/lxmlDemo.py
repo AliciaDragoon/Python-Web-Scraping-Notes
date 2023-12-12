c4b737ad2afd805979272a2a7397689508bb860c
AliciaDragoon <edogawaai@hotmail.com>
2023-12-12 20:46:54 +0800

    Add files via upload

--- PythonSpyder/day7/lxmlDemo.py
+++ PythonSpyder/day7/lxmlDemo.py
@@ -2,6 +2,7 @@ from lxml import etree
 
 f = open("index.html", mode="r", encoding="utf-8")
 content = f.read()  # 页面源代码
+
 # 默认情况下，pycharm不知道类型，所以没有代码提示
 page = etree.HTML(content)  # type: etree._Element
 # print(type(page))  # <class 'lxml.etree._Element'>
